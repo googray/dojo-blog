ce5c09db1ced933f78679e577e85a457a6ea8258
googray <shat.rom07@gmail.com>
2022-02-23 22:07:31 +0100

    add new func in bloglist

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,30 @@
-import React from "react";
-
-const handleClick = (e) => {
-  console.log("hello, roman", e);
-};
-const handleClickAgain = (name, e) => {
-  console.log("hello, " + name, e.target);
-};
+import React, { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <h1>Homepage</h1>
-      <button onClick={handleClick}>Click me</button>
-      {/* <button onClick={handleClickAgain}>Click me again</button> */}
-      <button onClick={(e) => handleClickAgain("roman", e)}>
-        Click me again
-      </button>
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {/* <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's blogs"
+      /> */}
     </div>
   );
 };
