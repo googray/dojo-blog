75adb037c196499a7238eec257c3ba10e3f5d864
googray <shat.rom07@gmail.com>
2022-02-26 13:05:53 +0100

    add func of adding new author blogs and body

---
+++ src/useFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCount = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { signal: abortCount.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("...could not fetch the data from that resorce");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
+        });
+    }, 1000);
+
+    return () => abortCount.abort();
+  }, [url]);
+  return { data, isPending, error };
+};
+
+export default useFetch;
